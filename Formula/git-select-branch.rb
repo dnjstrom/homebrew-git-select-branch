2a46e9f7f9c8d0103d7c5816324f9f28c55879ad
Daniel Ström <D@nielstrom.se>
2021-05-09 18:56:46 +0200

    v0.1.1

--- Formula/git-select-branch.rb
+++ Formula/git-select-branch.rb
@@ -4,9 +4,9 @@
 class GitSelectBranch < Formula
     desc "Tiny Rust CLI to checkout a recent git branch interactively"
     homepage "https://github.com/dnjstrom/git-select-branch"
-    url "https://github.com/dnjstrom/git-select-branch/releases/download/v0.1.0/git-select-branch-mac.tar.gz"
-    sha256 "27af17775c417ddfb7db44639b11485cba8c8ed6e9e7607f213ad6edb970de5a"
-    version "0.1.0"
+    url "https://github.com/dnjstrom/git-select-branch/releases/download/v0.1.1/git-select-branch-mac.tar.gz"
+    sha256 "eacabce53f2f66714396ad42281ffd6ead4f267811a16c6ee9d83fdc4c22cada"
+    version "0.1.1"
   
     def install
       bin.install "git-select-branch"
