6c4c1d452f214eadc2795af97d774f1ff2c6ee7f
Daniel Ström <D@nielstrom.se>
2023-11-15 20:00:40 +0100

    Update version and add support for arm and linux

--- Formula/git-select-branch.rb
+++ Formula/git-select-branch.rb
@@ -1,12 +1,26 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+# To quickly calculate the shasum of a remote file:
+#   curl -sL <URL> | shasum -a 256
+
 class GitSelectBranch < Formula
     desc "Tiny Rust CLI to checkout a recent git branch interactively"
     homepage "https://github.com/dnjstrom/git-select-branch"
-    url "https://github.com/dnjstrom/git-select-branch/releases/download/v0.2.0/git-select-branch-mac.tar.gz"
-    sha256 "f27c666a01a91c887803fdbe771a64db552145965c6fe866d6621090c183c452"
-    version "0.2.0"
+    version "0.2.3"
+    
+    on_macos do
+      on_arm do
+        url "https://github.com/dnjstrom/git-select-branch/releases/download/v0.2.3/git-select-branch-v0.2.3-aarch64-apple-darwin.tar.xz"
+        sha256 "b3ffb0ccca84aa5a0cca9fa450176657cd4be38d04fa22cf3f6de8e1ea80628e"
+      end
+      on_intel do
+        url "https://github.com/dnjstrom/git-select-branch/releases/download/v0.2.3/git-select-branch-v0.2.3-x86_64-apple-darwin.tar.xz"
+        sha256 "c09d61cfd87bb408daa95139177ccbdd82ac71a1c0fc2cba9f9aa308f8102c22"
+      end
+    end
+
+    on_linux do
+      url "https://github.com/dnjstrom/git-select-branch/releases/download/v0.2.3/git-select-branch-v0.2.3-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "50640c4cf52c67f892b67cf6a4d75364392a1e3692b19ed83315931ab5502c1b"
+    end
   
     def install
       bin.install "git-select-branch"
